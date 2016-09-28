93671a60bda225a5c7d27055a3ac1e8415dbe823
Jake Klovenski <jklovenski@utexas.edu>
2016-09-28 00:41:25 -0500

    DFS is written, will debug when not 1AM

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,7 @@ import java.io.*;
 public class Main {
 	
 	// static variables and constants only here.
-	Set<String> dict;
+	static Set<String> dict;
 	
 	
 	public static void main(String[] args) throws Exception {
@@ -58,12 +58,12 @@ public class Main {
 		String input = keyboard.nextLine();
 		if(input.equals("/quit"))
 		{
-			return new ArrayList();
+			return new ArrayList<String>();
 		}
 		int space_index = input.indexOf(" ");
 		String start = input.substring(0,space_index-1);
-		String end = input(space_indexi+1);
-		ArrayList<String> parsed = new ArrayList(2);
+		String end = input.substring(space_index+1);
+		ArrayList<String> parsed = new ArrayList<String>(2);
 		parsed.add(start);
 		parsed.add(end);
 		return parsed;
@@ -74,13 +74,58 @@ public class Main {
 		// Returned list should be ordered start to end.  Include start and end.
 		// Return empty list if no ladder.
 		// TODO more code
-		boolean discovered[dict.size()];
-		ArrayList ladder = new ArrayList();
+		boolean discovered[] = new boolean[dict.size()];
+		ArrayList<String> dictionary = new ArrayList<String>(dict);
 		Arrays.fill(discovered, false);
-		discoverd.set(dict.indexOf(start),true);
-		
-		
-		return null; // replace this line later with real return
+		discovered[dictionary.indexOf(start)] = true;
+		ArrayList<String> ladder = recurseDFSLadder(start, end, discovered, dictionary);
+		return ladder; // replace this line later with real return
+	}
+
+	private static ArrayList<String> recurseDFSLadder(String start, String end, boolean discovered[], ArrayList<String> dictionary)
+	{
+		boolean full_false[] = new boolean[dict.size()];
+		Arrays.fill(full_false, false);
+		if(distance(start,end) == 0)
+		{
+			ArrayList<String> val = new ArrayList<String>();
+			val.add(start);
+			return val;
+		}	
+		else if(!(discovered.equals(full_false)))
+		{
+			return new ArrayList<String>();
+		}
+		else
+		{
+			int min_diff = 1000;
+			int place = 0;
+			ArrayList<String> result = new ArrayList<String>();
+			while(result.isEmpty())
+			{
+				for(int i = 0; i < dict.size(); i++)
+				{
+					if(distance(dictionary.get(i), start) == 1 && !discovered[i] && distance(dictionary.get(i), end) < min_diff)
+					{
+						place = i;
+						min_diff = distance(dictionary.get(i), end);
+					}
+				}
+				if(min_diff == 1000)
+				{
+					return new ArrayList<String>();
+				}
+				else
+				{
+					discovered[place] = true;
+					result = recurseDFSLadder(dictionary.get(place), end, discovered,dictionary);
+				}
+				result.add(0,start);
+				return result;
+			}
+		}
+	
+		return null;
 	}
 	
     public static ArrayList<String> getWordLadderBFS(String start, String end) {
@@ -113,20 +158,17 @@ public class Main {
 	// TODO
 	// Other private static methods here
 	//
-	private static boolean oneAway(String a, String b)
+	private static int distance(String a, String b)
 	{
 		int delta = 0;
-		for(int i = 0; i < a.length; i++)
+		for(int i = 0; i < a.length(); i++)
 		{
 			if(a.charAt(i) != b.charAt(i))
 			{	
 				delta+=1;
 			}
 		}	
-		if(delta != 1)
-		{
-			return false;
-		}
-		return true;
+		return delta;	
 	}
+	
 }
